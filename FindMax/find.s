bf8cbe740e7a1ad3c66f52dc5c027657880b0cad
mantvydas <mantvydo@gmail.com>
2016-06-03 22:30:16 +0100

    formatting

--- FindMax/find.s
+++ FindMax/find.s
@@ -1,21 +1,22 @@
 .section .data
-   coinsList: .long 25,2,77,10,99,0
+   coinsList: .long 25,40,77,10,99,0
 
 .section .text
 
 .globl _start
 _start:
     mov $0, %edi
-    mov coinsList(,%edi,4), %eax ; eax = coinsList[0]
+    mov coinsList(,%edi,4), %eax # eax = coinsList[0]
     mov %eax, %ebx
 
 loopStart:
     cmp $0, %eax
-    je loopEnd ; if eax == 0 then loopEnd()
-    inc %edi ; edi++
+    je loopEnd # if eax == 0 then loopEnd:
+    inc %edi # edi++
     mov coinsList(,%edi,4), %eax
+
     cmp %ebx, %eax
-    jle loopStart
+    jle loopStart #if eax <= ebx then loopStart
     mov %eax, %ebx
     jmp loopStart
 
@@ -24,3 +25,6 @@ loopEnd:
     int $0x80
 
 pienas:
+    mov $1, %eax
+    mov $55, %ebx
+    jmp loopStart
