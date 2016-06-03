b9d18d0cae94cc95f472e8e8dd09f0f97034e16d
mantvydas <mantvydo@gmail.com>
2016-06-03 20:41:39 +0100

    playing with the flow of code execution

--- Exit/exit.s
+++ Exit/exit.s
@@ -1,11 +1,24 @@
 .section .data
-   coinsList: .byte 1,2,5,10,50,0
+   coinsList: .long 25,2,5,10,50,0
 
 .section .text
 
 .globl _start
-    _start:
-        mov $1, %eax
-        mov $4, %edi
-        mov coinsList(,%edi,1), %ebx
-        int $0x80
+_start:
+    movl $0, %edi
+    movl coinsList(,%edi,4), %eax
+    movl %eax, %ebx
+
+loopStart:
+    cmp $0, %ebx
+    jg loopEnd2
+
+loopEnd1:
+    mov $1, %eax
+    mov $11, %ebx
+    int $0x80
+
+loopEnd2:
+    mov $1, %eax
+    mov $22, %ebx
+    int $0x80
