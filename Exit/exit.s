571b7c1e0e729a524999b825db1a6ba5b51f62df
mantvydas <mantvydo@gmail.com>
2016-06-03 19:04:08 +0100

    access to a list through an indexed addressing mode

--- Exit/exit.s
+++ Exit/exit.s
@@ -1,9 +1,11 @@
 .section .data
+   coinsList: .byte 1,2,5,10,50,0
 
 .section .text
 
 .globl _start
-_start:
-    movl $1, %eax
-    movl $5, %ebx
-    int $0x80
+    _start:
+        mov $1, %eax
+        mov $4, %edi
+        mov coinsList(,%edi,1), %ebx
+        int $0x80
