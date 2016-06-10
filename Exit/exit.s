d8840702b145cf9ba28c2f8a7215bedd30cf08b8
Mantvydas Baranauskas <mantvydo@gmail.com>
2016-06-10 15:29:02 +0100

    Update exit.s
    housekeeping

--- Exit/exit.s
+++ Exit/exit.s
@@ -11,21 +11,3 @@ _start:
     mov %eax, %ebx
     mov $1, %eax
     int $0x80
-
-loopStart:
-    cmp $0, %ebx
-    mov $1, %eax
-    mov $12, %ebx
-
-loopEnd:
-    mov $1, %eax
-    mov $12, %ebx
-    int $0x80
-
-; loopStartas:
-;     mov $1, %eax
-;     jmp loopStartas2
-;
-; loopStartas2:
-;     mov $54, %ebx
-;     jmp loopStart
